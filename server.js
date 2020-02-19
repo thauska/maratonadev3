e833455918b2e919f8a48cc5b00c13f59e4990f7
Thayana Corrêa Mamoré <thayanacmamore@gmail.com>
2020-02-19 13:13:56 -0300

    configurando dados do formulário (somente em memória, antes do BD)

--- server.js
+++ server.js
@@ -5,15 +5,54 @@ const server = express()
 //configurar o servidor para apresentar arquivos estáticos
 server.use(express.static('public'))
 
+//habilitar body do formulário
+server.use(express.urlencoded({extended: true}))
+
 //configurando o template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true
 })
+
+//Agrupamento de dados: lista de doadores
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+"
+    },
+    {
+        name: "Cleiton Souza",
+        blood: "O+"
+    },
+    {
+        name: "Thayana Mamoré",
+        blood: "A+"
+    },
+    {
+        name: "Anouska Kunath",
+        blood: "B-"
+    },
+]
+
 
 //configurar a apresentação da página
 server.get("/", function(req, res) {
-    return res.render("index.html")
+    return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res) {
+    //pegar dados do formulário
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    donors.push({
+        name: name,
+        blood: blood
+    })
+
+    return res.redirect("/")
 })
 
 
